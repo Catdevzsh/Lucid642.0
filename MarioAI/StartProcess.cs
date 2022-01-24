e832c710a8172d6f93f95d2e8f76d21e13ce3295
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-01-24 13:06:04 -0800

    Lucid 64 2.0 - Request to upload 1.24.22

---
+++ MarioAI/StartProcess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioAI
+{
+    class StartProcess
+    {
+        public void start()
+        {
+            Console.WriteLine("Starting Project64.");
+            string path = Directory.GetCurrentDirectory();
+            string newPath = Path.GetFullPath(Path.Combine(path, @"..\..\..\Project64 2.3\"));
+            var proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                       // FileName = newPath + "Project64.exe",
+                        UseShellExecute = false,
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = false,
+                        CreateNoWindow = false
+                    }
+                };
+            //string path = Directory.GetCurrentDirectory();
+           // string newPath = Path.GetFullPath(Path.Combine(path, @"..\..\..\Project64 2.3\"));
+            Console.WriteLine(newPath);
+            proc.Start();
+            proc.StandardInput.WriteLine("cd " + newPath);
+            proc.StandardInput.WriteLine("Project64.exe");       
+            Console.WriteLine("Process Started.");
+            
+        }
+    }
+}
---
+++ MarioAI/_input.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using WindowsInput.Native;
+//using WindowsInput;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace MarioAI
+{
+    class _input
+    {
+        Keyboard keyboard = new Keyboard();
+        
+        public void Start_keyPress()
+        {
+            //When the key is press its flag is set to down so we need to set it back up and we also need to wait some time for the game to detect the input
+            keyboard.Send(Keyboard.ScanCodeShort.RETURN);
+            System.Threading.Thread.Sleep(100);
+            keyboard.SendKeyUp(Keyboard.ScanCodeShort.RETURN);
+         
+        }
+    }
+}
